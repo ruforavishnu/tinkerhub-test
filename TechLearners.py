367a461983e1c2d6dcb29e9015d2e360745ef596
Vishnu <vishnuajith310@gmail.com>
2020-03-18 13:43:10 +0530

    tinkerhub test completed

---
+++ TechLearners.py
@@ -0,0 +1,23 @@
+class TechLearnersAndMentors
+
+	category = ""
+	availableTime = 90;
+	currentStacks = []
+
+	def addStacks(expertise):
+		currentStacks.append(expertise)
+
+	def setMentorOrLearner(isLearnerOrMentor):
+		category = isLearnerOrMentor
+	def setAvailableTime():
+		if(typeOfTechLearnerAndMentor == "mentor"):
+			availableTime = 90
+
+
+
+	def getMentor(stack, time ):
+		#content
+
+
+
+
